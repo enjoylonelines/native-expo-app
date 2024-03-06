659a4e98b5b20e1e2ecdc673fd590716a235cd29
enjoylonelines <hobeom5022@naver.com>
2024-03-06 14:13:51 +0900

    컴포넌트 추가

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,14 +1,53 @@
+import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, Platform } from 'react-native'
 
 const MainScreen = () => {
   return (
-    <View>
-      <Text>MainScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={'default'} >
+        <Text style={styles.pageTitle}>ToDo App</Text>
+        <View style={styles.listView}>
+          <Text style={styles.listTitle} >할 일</Text>
+        </View>
+        <View style={styles.separator} />
+        <View>
+          <Text>완료된 일</Text>
+        </View>
+      </StatusBar>
+    </SafeAreaView>
   )
 }
 
-export default MainScreen
+export default MainScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 20 : 0,
+    backgroundColor: '#f7f8fa'
+  },
+  pageTitle: {
+    marginBottom: 35,
+    paddingHorizontal: 15,
+    fontSize: 54,
+    fontWeight: '600'
+  },
+  separator: {
+    marginHorizontal: 10,
+    marginTop: 25,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0,0,0,0.2)'
+  },
+  listView: {
+    flex: 1,
+  },
+  listTitle: {
+    marginBottom: 25,
+    paddingHorizontal: 15,
+    fontSize: 41,
+    fontWeight: '500'
+  }
+});
+
