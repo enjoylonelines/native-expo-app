e061412878064ef16d5e243e820f8fb1381be960
enjoylonelines <hobeom5022@naver.com>
2024-03-08 16:23:45 +0900

    loginscreen

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import MainScreen  from './screens/MainScreen'
+import LoginScreen from './screens/LoginScreen';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import app from './firebase'
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <MainScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen options={{ headerShown: false }} name='Main' component={MainScreen} />
+          <Stack.Screen name='Screen' component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWOkTQqDR6TZHBxCoZflA2iSOXfeDR0hY",
+  authDomain: "react-native-todo-app-51ace.firebaseapp.com",
+  projectId: "react-native-todo-app-51ace",
+  storageBucket: "react-native-todo-app-51ace.appspot.com",
+  messagingSenderId: "1014650882824",
+  appId: "1:1014650882824:web:989707cb7713bc4de7fabd"
+};
+
+const app = initializeApp(firebaseConfig);
